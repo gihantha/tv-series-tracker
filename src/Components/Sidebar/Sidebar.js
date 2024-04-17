ec653d6f4f521199c3a8c30b65a5a5f1d97de25f
gihantha <gihantha01@gmail.com>
2024-04-17 20:40:53 +0530

    Sidebar Component Implementing and CSS initalization

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import "./Sidebar.css";
+import next_page_button_icon from "../../Assets/next page.png";
+import filter_button_icon from "../../Assets/filter button.png";
+import advanced_button_icon from "../../Assets/advanced icon.png";
+import sidebar_image from "../../Assets/joker.jpg";
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <p>TV Shows</p>
+        {/* <div className="sidebar-action-buttons"> */}
+          <div className="next-page-button">
+            <img src={next_page_button_icon} alt="Next Page Button Icon" />
+          </div>
+          <div className="filter-button">
+            <img src={filter_button_icon} alt="Filter Button Icon" />
+          </div>
+          <div className="advanced-button">
+            <img src={advanced_button_icon} alt="Advanced Button Icon" />
+          </div>
+        {/* </div> */}
+      </div>
+      <p>There are 4,021 people watching 1,336 shows right now!</p>
+      <div className="sidebar-menu">
+        <ul>
+          <li>Trending</li>
+          <li>Popular</li>
+          <li>Favourited</li>
+          <li>Watched</li>
+          <li>Collected</li>
+          <li>Anticipated</li>
+        </ul>
+      </div>
+      <div className="sidebar-img">
+        <img src={sidebar_image} alt="sidebar image" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
