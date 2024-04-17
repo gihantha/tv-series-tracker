4f47283ddce39c4217093ada98bab96130d77951
gihantha <gihantha01@gmail.com>
2024-04-17 19:03:59 +0530

    Setting up project structure and basic dependencies.

---
+++ src/Components/Actionbar/Actionbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Actionbar.css'
+
+const Actionbar = () => {
+  return (
+    <div>
+      Actionbar
+    </div>
+  )
+}
+
+export default Actionbar
---
+++ src/Components/DisplayCard/DisplayCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayCard = () => {
+  return (
+    <div>
+      DisplayCard
+    </div>
+  )
+}
+
+export default DisplayCard
